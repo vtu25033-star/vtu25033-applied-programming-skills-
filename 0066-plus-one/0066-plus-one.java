af73ac76775edebf46a097a12166cfb1ce01c354
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-03 18:36:51 +0530

    Time: 0 ms (100%), Space: 43.6 MB (39.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,25 +1,40 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        int carry = 1;
-        for(int i=digits.length-1;i>=0;i--){
-            if(digits[i]+carry <= 9){
-                ans.add(digits[i]+carry);
-                carry=0;
-            }
-            else{
-                ans.add(0);
-                carry=1;
-            }
-        }
-        if(carry == 1) ans.add(1);
-        
-        Collections.reverse(ans);
+         // 1st method
 
-        int[] result = new int[ans.size()];
-        for(int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i);
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // int carry = 1;
+        // for(int i=digits.length-1;i>=0;i--){
+        //     if(digits[i]+carry <= 9){
+        //         ans.add(digits[i]+carry);
+        //         carry=0;
+        //     }
+        //     else{
+        //         ans.add(0);
+        //         carry=1;
+        //     }
+        // }
+        // if(carry == 1) ans.add(1);
+        
+        // Collections.reverse(ans);
+
+        // int[] result = new int[ans.size()];
+        // for(int i = 0; i < ans.size(); i++) {
+        //     result[i] = ans.get(i);
+        // }
+        // return result;
+          
+          // 2nd method
+
+        for(int i = digits.length-1;i>-1;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        return result;
+        int[] answer = new int[digits.length+1];
+        answer[0] = 1;
+        return answer;
     }
 }
