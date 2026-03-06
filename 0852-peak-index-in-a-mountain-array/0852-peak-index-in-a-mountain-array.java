e0df570b7dd6a62f5536cfef6e8a5d9aaef1ea49
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-06 18:58:33 +0530

    Time: 0 ms (100%), Space: 80.6 MB (6.84%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int low = 1,high = arr.length - 2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+            else if(arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return 143;
+    }
+}                                                                                                                   
