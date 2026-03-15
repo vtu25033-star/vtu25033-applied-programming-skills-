c868d25056fbff3a6b8232a7d9abad226ee52353
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-15 09:44:16 +0530

    Time: 1 ms (87.49%), Space: 42.7 MB (57.45%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,16 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        
-        long count = 0;
-        if(n>=1000){
-            count+=Math.min(n,999999) -999;
-        }
-         if(n>=1000000){
-            count+=Math.min(n,999999999) -999999*2;
-        }
-         if(n>=1000000000){
-            count+=(n-999999999) *3;
-        }
-        return (int)count;
+        if(n<999) return 0;
+        else return n - 999;
     }
 }
