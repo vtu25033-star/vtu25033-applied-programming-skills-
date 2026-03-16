05d46a0912b2c95c76551a93ecf81376719ef9f4
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-16 11:57:57 +0530

    Time: 0 ms (100%), Space: 43 MB (75.66%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -4,21 +4,25 @@ class Solution {
         int n = arr.length,m = arr[0].length;
         int firstrow = 0,firstcol = 0,lastrow = n - 1,lastcol = m-1;
         while(firstrow<=lastrow  &&  firstcol<=lastcol){
+            // FIRST ROW 
             for(int j=firstcol;j<=lastcol;j++){
                 ans.add(arr[firstrow][j]);
             }
             firstrow++;
             if(firstrow>lastrow || firstcol>lastcol) break;
+            // LAST COL
             for(int i=firstrow;i<=lastrow;i++){
                 ans.add(arr[i][lastcol]);
             }
             lastcol--;
             if(firstrow>lastrow || firstcol>lastcol) break;
+            // LAST ROW 
             for(int j=lastcol;j>=firstcol;j--){
                 ans.add(arr[lastrow][j]);
             }
             lastrow--;
             if(firstrow>lastrow || firstcol>lastcol) break;
+            // FIRST COL
             for(int i=lastrow;i>=firstrow;i--){
                 ans.add(arr[i][firstcol]);
             }
