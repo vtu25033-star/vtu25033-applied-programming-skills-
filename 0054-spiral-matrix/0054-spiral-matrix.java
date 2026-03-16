d0b134abd536d37e2fb30fe3111f174c292bb447
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-16 11:40:11 +0530

    Time: 0 ms (100%), Space: 43.1 MB (56.92%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+        List<Integer> ans = new ArrayList<Integer>();
+        int n = arr.length,m = arr[0].length;
+        int firstrow = 0,firstcol = 0,lastrow = n - 1,lastcol = m-1;
+        while(firstrow<=lastrow  &&  firstcol<=lastcol){
+            for(int j=firstcol;j<=lastcol;j++){
+                ans.add(arr[firstrow][j]);
+            }
+            firstrow++;
+            if(firstrow>lastrow || firstcol>lastcol) break;
+            for(int i=firstrow;i<=lastrow;i++){
+                ans.add(arr[i][lastcol]);
+            }
+            lastcol--;
+            if(firstrow>lastrow || firstcol>lastcol) break;
+            for(int j=lastcol;j>=firstcol;j--){
+                ans.add(arr[lastrow][j]);
+            }
+            lastrow--;
+            if(firstrow>lastrow || firstcol>lastcol) break;
+            for(int i=lastrow;i>=firstrow;i--){
+                ans.add(arr[i][firstcol]);
+            }
+            firstcol++;
+        }
+        return ans;
+    }
+}
