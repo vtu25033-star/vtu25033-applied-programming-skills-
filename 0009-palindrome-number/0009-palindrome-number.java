bdb531be15e4167e02f1f6442813941799d269a0
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-20 12:26:58 +0530

    Time: 4 ms (99.89%), Space: 45.9 MB (67.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int rev = 0;
+        int n = x;
+        while(n!=0){
+            int rem = n % 10;
+            rev = (rev * 10) + rem;
+            n = n/10;
+        }
+        if(x == rev) return true;
+        else return false;
+    }
+}
