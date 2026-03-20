b05ac4cdcffaf58a2a81b1ae41c07f93e6038575
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-20 20:17:34 +0530

    Time: 0 ms (100%), Space: 44.6 MB (87.46%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -3,18 +3,18 @@ class Solution {
         long largest = Long.MIN_VALUE;
         long second = Long.MIN_VALUE;
         long third = Long.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>largest){
+        for(int i:nums){
+            if(i>largest){
                 third = second;
                 second = largest;
-                largest = nums[i];
+                largest = i;
             }
-            else if (nums[i] > second && nums[i] < largest){
+            else if (i > second && i != largest){
                 third = second; 
-                second = nums[i];}
-            else if(nums[i] > third && nums[i] < second) third = nums[i];
+                second = i;}
+            else if(i > third && i != largest && i != second) third = i;
         }
-        if(third == Long.MIN_VALUE) return (int)largest;
-        else return (int)third;
+        return (third == Long.MIN_VALUE) ? (int)largest : (int)third;
+    
     }
 }
