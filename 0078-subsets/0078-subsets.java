6967721fc134fd96d22d402ccc18a6f98538aadc
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-30 22:38:34 +0530

    Time: 1 ms (88.18%), Space: 43.8 MB (95.93%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        subsets(new ArrayList<>(),nums,0,list);
+        return list;
+    }
+    public void subsets(ArrayList<Integer> ans,int[] nums,int idx,List<List<Integer>> list){
+        if(idx==nums.length){
+            list.add(new ArrayList<>(ans));
+            return;
+        }
+        ans.add(nums[idx]);
+        subsets(ans,nums,idx+1,list);
+
+        ans.remove(ans.size() - 1);
+        subsets(ans,nums,idx+1,list);
+    }
+}
