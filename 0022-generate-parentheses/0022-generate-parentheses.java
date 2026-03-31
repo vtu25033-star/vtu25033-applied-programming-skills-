d60b1178dd233e3df39b41b06b43f9e638551738
vtu25033-star <vtu25033@veltech.edu.in>
2026-03-31 21:21:09 +0530

    Time: 2 ms (71.43%), Space: 44.8 MB (39.5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void generate(int n,int l,int r,String s,List<String> ans) {
+        if(l+r==2*n) {
+            ans.add(s);
+            return;
+        }
+        if(l<n) generate(n,l+1,r,s+"(",ans);
+        if(r<l) generate(n,l,r+1,s+")",ans);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        generate(n,0,0,"",ans);
+        return ans;
+    }
+}
