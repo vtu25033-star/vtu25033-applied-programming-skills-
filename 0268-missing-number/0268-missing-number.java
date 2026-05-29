dec1fb5debeee15e2c04aa763f68ff0dfe1a606d
vtu25033-star <vtu25033@veltech.edu.in>
2026-05-29 17:03:25 +0530

    Time: 1 ms (29.14%), Space: 47.2 MB (80.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,28 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        // int n = nums.length;
+        // int sum = n*(n+1)/2;
+        // int Arraysum = 0;
+        // for(int num:nums) Arraysum+=num;
+        // return sum-Arraysum;
+
+        int i=0;
         int n = nums.length;
-        int sum = n*(n+1)/2;
-        int Arraysum = 0;
-        for(int num:nums) Arraysum+=num;
-        return sum-Arraysum;
+        while(i<nums.length){
+            if(nums[i] == i || nums[i] == n) i++;
+            else{
+                int idx = nums[i];
+                swap(nums,i,idx);
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != j) return j;
+        }
+        return n;
+    }
+    private void swap (int[] arr,int i,int idx){
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
     }
 }
