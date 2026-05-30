89345f3140e046a7e68a34bf39f143715500d85a
vtu25033-star <vtu25033@veltech.edu.in>
2026-05-30 10:37:44 +0530

    Time: 1 ms (100%), Space: 71.3 MB (44.74%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while(i<nums.length){
+            int idx = nums[i] - 1;
+            if(nums[i] < 0 || nums[i] == 0 || nums[i] == i+1 || nums[i] >= n) {
+                i++;
+            }
+            else{
+                if(nums[idx] == nums[i]) i++;
+                else
+                {   int temp = nums[i];
+                    nums[i] = nums[idx];
+                    nums[idx] = temp;
+                }
+            }
+        }
+
+        for(int j = 0;j<nums.length;j++){
+            if(nums[j] != j+1) return j+1;
+        }
+        return n+1;
+    }
+}
