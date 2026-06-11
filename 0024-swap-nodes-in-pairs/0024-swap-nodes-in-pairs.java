9d04d1460e983402723e32cbfad45b5008032517
vtu25033-star <vtu25033@veltech.edu.in>
2026-06-11 18:08:04 +0530

    Time: 0 ms (100%), Space: 43 MB (65.49%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+       ListNode d1 = new ListNode(1);
+       ListNode temp = head;
+       ListNode t1 = d1;
+       while(temp!=null && temp.next!=null){
+            ListNode nextPair = temp.next.next;
+            t1.next = temp.next;
+            t1 = t1.next;
+            t1.next = temp;
+            t1 = t1.next; 
+            temp = nextPair;
+       }
+       t1.next = temp;
+
+       return d1.next;
+
+    }
+}
