3bdb87af3f7e7dad0baf7f2f8cc6b780aec0edb4
vtu25033-star <vtu25033@veltech.edu.in>
2026-06-11 16:58:01 +0530

    Time: 0 ms (100%), Space: 46.5 MB (34.3%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         ListNode odd = new ListNode(1);
@@ -28,6 +18,5 @@ class Solution {
         te.next = null;
 
         return odd.next;
-
     }
 }
