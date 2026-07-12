62b929c96d8476d0c614b81e79d583f109467878
vtu25033-star <vtu25033@veltech.edu.in>
2026-07-12 18:20:01 +0530

    Time: 2 ms (18.03%), Space: 42.8 MB (42.97%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -4,32 +4,59 @@ class MyStack {
         
     }
     
-    public void push(int x) {
-        q.add(x);
-    }
-    
-    public int pop() {
-        int n = q.size();
+    public void push(int x) {  // O(n)
+       q.add(x);
+       int n = q.size();
        for(int i=1;i<=n-1;i++){
-            q.add(q.remove());
-        }
-        return q.remove();
-    }
-    
-    public int top() {
-        int n = q.size();
-       for(int i=1;i<=n-1;i++){
-            q.add(q.remove());
-        }
-        int p = q.peek();
         q.add(q.remove());
-        return p;
+       }
+    }
+    
+    public int pop() { // O(1)
+       return q.remove();
+    }
+     
+    public int top() {  // O(1)
+       return q.peek();
     }
     
     public boolean empty() {
         return (q.size()==0);
     }
 }
+
+// class MyStack {
+//     Queue<Integer> q = new LinkedList<>();
+//     public MyStack() {
+        
+//     }
+    
+//     public void push(int x) {  // O(1)
+//         q.add(x);
+//     }
+    
+//     public int pop() { // O(n)
+//         int n = q.size();
+//        for(int i=1;i<=n-1;i++){
+//             q.add(q.remove());
+//         }
+//         return q.remove();
+//     }
+     
+//     public int top() {  // O(n)
+//         int n = q.size();
+//        for(int i=1;i<=n-1;i++){
+//             q.add(q.remove());
+//         }
+//         int p = q.peek();
+//         q.add(q.remove()); //imp
+//         return p;
+//     }
+    
+//     public boolean empty() {
+//         return (q.size()==0);
+//     }
+// }
 
 /**
  * Your MyStack object will be instantiated and called as such:
