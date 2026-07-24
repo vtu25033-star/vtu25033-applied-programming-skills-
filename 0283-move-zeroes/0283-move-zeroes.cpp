9bdeda0a20b019517cd69766959bdd6508d47d02
vtu25033-star <vtu25033@veltech.edu.in>
2026-07-24 11:14:53 +0530

    Time: 4 ms (10.74%), Space: 23.9 MB (19.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
         int i = 0,j=0;
-        size_t length = std::size(arr);
-        while(j<length){
+        while(j<arr.size()){
             if(arr[j]!=0){
                 int temp = arr[i];
                 arr[i] = arr[j];
