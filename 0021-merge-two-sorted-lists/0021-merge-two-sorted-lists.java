ff14c21067d00fb77728519e4f0f5ea2464586a8
vtu25033-star <vtu25033@veltech.edu.in>
2026-07-27 11:27:32 +0530

    Time: 0 ms (100%), Space: 44.4 MB (20.81%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,28 +8,54 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
+//         ListNode d = new ListNode(0);
+//         ListNode k = d;
+//         ListNode i = h1;
+//         ListNode j = h2;
+        
+//         while(i != null && j != null){
+//             if(i.val < j.val){
+//                 k.next = i;
+//                 i = i.next;
+//             }
+//             else{
+//                 k.next = j;
+//                 j = j.next;
+//             }
+//             k = k.next;
+//         }
+//         if(i == null) k.next = j;
+//         if(j == null) k.next = i;
+        
+        
+//         return d.next;
+//     }
+// }
+
 class Solution {
     public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
-        ListNode d = new ListNode(0);
-        ListNode k = d;
-        ListNode i = h1;
-        ListNode j = h2;
-        
-        while(i != null && j != null){
-            if(i.val < j.val){
-                k.next = i;
-                i = i.next;
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        ListNode a = h1;
+        ListNode b = h2;
+
+        while(a != null && b != null){
+            if(a.val>b.val){
+                temp.next = b;
+                b = b.next;
             }
             else{
-                k.next = j;
-                j = j.next;
+                temp.next = a;
+                a = a.next;
             }
-            k = k.next;
+            temp = temp.next;
         }
-        if(i == null) k.next = j;
-        if(j == null) k.next = i;
-        
-        
-        return d.next;
+        if(a==null) temp.next = b;
+        if(b==null) temp.next = a;
+
+        return dummy.next;
+
     }
 }
