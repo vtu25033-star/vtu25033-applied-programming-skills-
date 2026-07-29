8457a74b911172622e63483dca3e6a17079e4d81
vtu25033-star <vtu25033@veltech.edu.in>
2026-07-29 13:55:53 +0530

    Time: 6 ms (35.43%), Space: 93.9 MB (96.39%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -23,9 +23,7 @@ class Solution {
     }
     public ListNode middle(ListNode head){
         ListNode slow = head;
-        ListNode slow1 = head;
         ListNode fast = head;
-        ListNode first = head;
         while(fast!=null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
