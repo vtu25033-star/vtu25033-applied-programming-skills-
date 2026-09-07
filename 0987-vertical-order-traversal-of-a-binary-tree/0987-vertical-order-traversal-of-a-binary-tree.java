7c2b2f893da6f1fd33e2f66c95db990b165e0197
vtu25033-star <vtu25033@veltech.edu.in>
2026-09-07 11:32:51 +0530

    Time: 4 ms (58.2%), Space: 44.5 MB (14.96%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class NodeInfo {
+        TreeNode node;
+        int row;
+        int col;
+
+        NodeInfo(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Map<Integer, Map<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<NodeInfo> queue = new LinkedList<>();
+        queue.offer(new NodeInfo(root, 0, 0));
+
+        while (!queue.isEmpty()) {
+            NodeInfo curr = queue.poll();
+            TreeNode node = curr.node;
+            int row = curr.row;
+            int col = curr.col;
+
+            map.putIfAbsent(col, new TreeMap<>());
+            map.get(col).putIfAbsent(row, new PriorityQueue<>());
+            map.get(col).get(row).offer(node.val);
+
+            if (node.left != null) {
+                queue.offer(new NodeInfo(node.left, row + 1, col - 1));
+            }
+            if (node.right != null) {
+                queue.offer(new NodeInfo(node.right, row + 1, col + 1));
+            }
+        }
+
+        for (Map<Integer, PriorityQueue<Integer>> colMap : map.values()) {
+            List<Integer> colList = new ArrayList<>();
+            for (PriorityQueue<Integer> pq : colMap.values()) {
+                while (!pq.isEmpty()) {
+                    colList.add(pq.poll());
+                }
+            }
+            result.add(colList);
+        }
+
+        return result;
+    }
+}
