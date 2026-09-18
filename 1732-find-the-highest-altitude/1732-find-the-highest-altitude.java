50f4c32229b49e084395211bb6049d40b484de77
vtu25033-star <vtu25033@veltech.edu.in>
2026-09-18 16:17:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (56.52%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int currentAltitude = 0;
+        
+        for (int g : gain) {
+            currentAltitude += g;
+            if (currentAltitude > maxAltitude) {
+                maxAltitude = currentAltitude;
+            }
+        }
+        
+        return maxAltitude;
+    }
+}
