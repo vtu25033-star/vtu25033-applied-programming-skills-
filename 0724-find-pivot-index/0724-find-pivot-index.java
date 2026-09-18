f88b1120363ebc4bdf072d952e0370d78a637f6e
vtu25033-star <vtu25033@veltech.edu.in>
2026-09-18 12:37:47 +0530

    Time: 1 ms (96.26%), Space: 47.6 MB (8.58%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,16 +3,12 @@ class Solution {
         int total = 0;
         for (int x : nums)
             total += x;
-
         int left = 0;
-
         for (int i = 0; i < nums.length; i++) {
             if (left == total - left - nums[i])
                 return i;
-
             left += nums[i];
         }
-
         return -1;
     }
 }
